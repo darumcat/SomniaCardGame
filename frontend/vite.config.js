b32ae748e68a589a6dc0b01c420cde070b6546f6
darumcat <alexs14@list.ru>
2025-04-30 23:09:49 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,31 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  root: './',
-  publicDir: 'public',
   plugins: [
     react(),
-    nodePolyfills()
-  ],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './public/index.html'
+    nodePolyfills({
+      include: ['crypto', 'stream', 'util', 'assert', 'http', 'https'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
       }
+    })
+  ],
+  define: {
+    global: 'globalThis',
+    'process.env': {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 })
