bc4c0cff6451a14c2b3948d9633031be4a681e67
darumcat <alexs14@list.ru>
2025-04-30 21:39:52 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  publicDir: 'public', // Явно указываем папку с static-файлами
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
+    rollupOptions: {
+      input: {
+        main: './public/index.html' // Явный путь к входному файлу
+      }
+    }
   },
-  define: {
-    'process.env': {}
-  }
-});
+  plugins: [react()]
+})
