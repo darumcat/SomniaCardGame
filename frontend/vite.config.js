ae22f39533ad0314b43ac1e06ee2e6af1a84a2ae
darumcat <alexs14@list.ru>
2025-04-30 23:57:12 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname, './'),
-  publicDir: path.resolve(__dirname, 'public'),
-  plugins: [
-    react(),
-    nodePolyfills()
-  ],
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html')
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['ethers'],
+    esbuildOptions: {
+      target: 'es2020'
     }
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+  build: {
+    target: 'es2020',
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
 })
