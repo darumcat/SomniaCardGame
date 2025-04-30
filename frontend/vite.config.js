86550ea901fadb28868bbbd01a90c4163c4812ec
darumcat <alexs14@list.ru>
2025-04-30 23:42:07 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  root: './frontend',  // Явно указываем корень проекта
+  root: './',
   publicDir: 'public',
   plugins: [
     react(),
@@ -12,9 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: './frontend/public/index.html'  // Полный путь
-      }
+      input: './public/index.html' // Указываем относительный путь
     }
   }
 })
