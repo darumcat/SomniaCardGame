191cbf91913cff66a39c89d6f286a0efcf034b5a
darumcat <alexs14@list.ru>
2025-04-30 22:43:35 +1100

    Update NetworkChecker.jsx

--- frontend/src/components/NetworkChecker.jsx
+++ frontend/src/components/NetworkChecker.jsx
@@ -1,34 +1,35 @@
-import { useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { useWeb3 } from '../context/Web3Context';
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useWeb3 } from '../context/Web3Context'
 
 const NetworkChecker = () => {
-  const { account } = useWeb3();
+  const { account } = useWeb3()
 
   useEffect(() => {
     const checkNetwork = async () => {
       if (window.ethereum && account) {
-        const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+        const chainId = await window.ethereum.request({ method: 'eth_chainId' })
         if (chainId !== '0xc4d8') { // 50312 в hex
           toast.error('Пожалуйста, подключитесь к Somnia Testnet (ChainID: 50312)', {
             position: "top-center",
             autoClose: false,
             closeOnClick: false,
             draggable: false
-          });
+          })
         }
       }
-    };
+    }
 
-    checkNetwork();
-    window.ethereum?.on('chainChanged', checkNetwork);
+    checkNetwork()
+    window.ethereum?.on('chainChanged', checkNetwork)
 
     return () => {
-      window.ethereum?.removeListener('chainChanged', checkNetwork);
-    };
-  }, [account]);
+      window.ethereum?.removeListener('chainChanged', checkNetwork)
+    }
+  }, [account])
 
-  return null;
-};
+  return null
+}
 
+export default NetworkChecker
 export default NetworkChecker;
