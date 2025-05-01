ae4d4b10c1713f1ad61b7489e162c24c96fb6802
darumcat <alexs14@list.ru>
2025-05-01 11:20:19 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',  // Четко указываем выходную директорию
+    emptyOutDir: true,
+    target: 'es2020',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')  // Явное указание на входной файл
+      }
+    }
+  },
   optimizeDeps: {
     include: ['ethers'],
     esbuildOptions: {
       target: 'es2020'
-    }
-  },
-  build: {
-    target: 'es2020',
-    commonjsOptions: {
-      transformMixedEsModules: true
     }
   }
 })
