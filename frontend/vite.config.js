de62e8789b7c60674bff2350d2baaa4c9f570086
darumcat <alexs14@list.ru>
2025-05-01 11:36:06 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['ethers'],
-    esbuildOptions: {
-      target: 'es2020'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'ethers': path.resolve(__dirname, 'node_modules/ethers/lib.esm.js')
     }
   },
   build: {
+    outDir: 'dist',
     target: 'es2020',
     commonjsOptions: {
       transformMixedEsModules: true,
