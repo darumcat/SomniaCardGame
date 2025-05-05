0394a218cbd7064648a505b557881c94ae00a0f9
darumcat <alexs14@list.ru>
2025-05-05 14:11:01 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,16 +7,19 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@utils': path.resolve(__dirname, './src/utils'), // Добавлено
+      '@utils': path.resolve(__dirname, './src/utils'),
       'ethers': path.resolve(__dirname, './node_modules/ethers')
-    }
+    },
+    extensions: ['.js', '.jsx']
   },
   optimizeDeps: {
     include: [
       'react',
       'react-dom',
       'ethers',
-      'react-toastify'
+      'react-toastify',
+      'framer-motion', // Добавлено
+      'bad-words'     // Добавлено
     ],
     exclude: ['js-big-decimal']
   },
