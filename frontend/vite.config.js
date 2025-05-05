299e41f980d845decb0f6ec47b4b96e50bb0bdd5
darumcat <alexs14@list.ru>
2025-05-05 12:07:00 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './', // Явно указываем корень проекта
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html') // Явный путь к index.html
+      external: [
+        'react-toastify',
+        'ethers'
+      ]
     }
   }
 })
