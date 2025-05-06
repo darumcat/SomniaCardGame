836bbca8996f9dba46fd2e33586e206d9dbce2fe
darumcat <alexs14@list.ru>
2025-05-07 08:48:40 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@context': path.resolve(__dirname, './src/context')
+    }
+  },
   build: {
     sourcemap: false,
     minify: 'esbuild',
