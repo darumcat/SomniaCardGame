4de508cf0962abdb587a376143385610eef04f10
darumcat <alexs14@list.ru>
2025-05-06 16:47:58 +1100

    Update Web3Context.jsx

--- frontend/src/context/Web3Context.jsx
+++ frontend/src/context/Web3Context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { CONTRACT_ADDRESSES } from '../utils/contracts';
+import { CONTRACT_ADDRESSES, ABI_PATHS } from '../utils/contracts';
 
 const Web3Context = createContext();
 
