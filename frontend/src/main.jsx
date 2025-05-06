dd1a37797df9933229184ef4259b91955bcd2202
darumcat <alexs14@list.ru>
2025-05-06 11:01:04 +1100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css'
-import './animations.css' // Добавьте эту строку
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
