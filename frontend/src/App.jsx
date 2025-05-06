decbcaeaac7bf92386cc9020632f0cf9d2b12eac
darumcat <alexs14@list.ru>
2025-05-06 11:45:26 +1100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const SOMNIA_CONFIG = {
     symbol: 'STT',
     decimals: 18
   },
-  rpcUrls: ['https://dream-rpc.somnia.network/'],
+  rpcUrls: ['https://shannon-explorer.somnia.network/'], 
   blockExplorerUrls: ['https://shannon-explorer.somnia.network/']
 };
 
