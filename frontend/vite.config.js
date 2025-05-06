87e409a1ffc5d40fea3bd33af10f3283e5fb8512
darumcat <alexs14@list.ru>
2025-05-06 12:55:48 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,
     minify: 'esbuild',
-    target: 'esnext'
+    target: 'esnext',
+    outDir: 'dist',
+    emptyOutDir: true
   },
   server: {
     port: 3000,
-    strictPort: true
+    strictPort: true,
+    open: true
+  },
+  define: {
+    'process.env': {}
   }
-})
+});
