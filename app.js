44a1d909d9794822ec63c759125fdea4a3c08fc2
darumcat <alexs14@list.ru>
2025-05-13 18:21:39 +1100

    ...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { useState, useEffect } = React;
 
 // Константы
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzYBo2AEVQBkpvCdFrqyf0hFZGRmCRS_2-jGnu7cj3EuSwUEsuRR_awiCq-zrjYjb1cCQ/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwQJqn2P7Q3O-bogDB0J8yncZVCD59Tmm4m6RH653GTDfrOKUc7TBm2k38XOTCb5kozYA/exec";
 const SHEET_ID = "174UJqeEN3MXeRkQNdnaK8V6bquo6Ce5rzsumQ9OWO3I";
 const NFT_CONTRACT_ADDRESS = "0xdE3252Ba19C00Cb75c205b0e4835312dF0e8bdDF";
 const USDCARD_CONTRACT_ADDRESS = "0x0Bcbe06d75491470D5bBE2e6F2264c5DAa55621b";
